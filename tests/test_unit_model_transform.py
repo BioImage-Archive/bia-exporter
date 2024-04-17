36ab05fd811b7cb1024a205d25357a09a526c4bc
Francois <fsherwood@ebi.ac.uk>
2024-04-17 16:19:12 +0100

    added vscode settings to repo

--- tests/test_unit_model_transform.py
+++ tests/test_unit_model_transform.py
@@ -3,24 +3,17 @@ from bia_export.models import ExportImage
 from bia_export.scli import rw_client
 
 from bia_integrator_api import models as api_models
-import unittest
-from unittest.mock import patch, Mock
 
 
 def test_bia_image_to_export_image(
     bia_image: api_models.BIAImage,
     bia_study: api_models.BIAStudy,
     website_image: ExportImage,
-    mocker,
 ):
-    # mocker.patch("rw_client.get_image_acquisition").return_value.specimen_uuid = (
-    #     "Valid UUID"
-    # )
-    # mocker.patch("..scli.rw_client.get_specimen").return_value.biosample_uuid = (
-    #     "Valid UUID"
-    # )
-    # mocker.patch("..scli.rw_client.get_image_acquisition").return_value = "Valid UUID"
-
+    # Test currently relies on the example data not having any of:
+    # BioSample, Specimen and ImageAcquisition
+    # in order to not call the api.
+    # TODO: use mocking to avoid api calls
     export_image = bia_image_to_export_image(
         image=bia_image, study=bia_study, use_cache=False
     )
